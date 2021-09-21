0787e170edc8c84677be96a63a4429d73f0ad3a0
shahrom3377@gmail.com <shahrom3377@gmail.com>
2021-09-21 06:43:10 +0300

    some codes in second day

---
+++ day_2.cpp
@@ -0,0 +1,65 @@
+// C++ Programming Example No.1
+// ----codescracker.com----
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Compiler, I am C++";
+    cout<<endl;
+    return 0;
+}
+//it is easy but not bad
+#include<iostream>
+using namespace std;
+int main()
+{
+    int val;
+    cout<<"Enter the Number: ";
+    cin>>val;
+    cout<<"\nThe Value is "<<val;
+    cout<<endl;
+    return 0;
+}
+// (in 2-day )
+
+
+#include<iostream>
+using namespace std;
+
+int add () {
+
+
+
+}
+int main () {
+	int age=23;
+	std::cout<<"Age="<<age;
+
+
+
+
+	return 0;
+
+}
+//(in 2-day)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Prints sum 
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}// this is better than first day
+
